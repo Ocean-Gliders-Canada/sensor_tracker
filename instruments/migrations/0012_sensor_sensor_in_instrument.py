b477d9d17a15562f494dff68c8260b7dad0605b6
xaingling <neolx199@gmail.com>
2019-07-03 12:41:26 -0300

    migrate file updated and added quote for href

--- instruments/migrations/0012_sensor_sensor_in_instrument.py
+++ instruments/migrations/0012_sensor_sensor_in_instrument.py
@@ -6,13 +6,59 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('instruments', '0011_auto_20190619_1703'),
     ]
 
     operations = [
         migrations.RunSQL(
-            "insert into instruments_sensoroninstrument(instrument_id, sensor_id,start_time) select s.instrument_id, s.id, NOW() from instruments_sensor as s"
+            "update instruments_sensor set created_date = modified_date where created_date is null;"
+        ),
+        migrations.RunSQL(
+            """
+with e as (
+    SELECT
+      id,
+      created_date,
+      (
+        SELECT p2.created_date
+        FROM instruments_sensor p2
+        WHERE p2.created_date IS NOT NULL AND p2.id <= p1.id
+        ORDER BY p2.id DESC
+        LIMIT 1
+      ) AS new_value
+    FROM instruments_sensor p1
+    ORDER BY id
+)
+update instruments_sensor
+set created_date = e.new_value
+from e
+where instruments_sensor.id = e.id and instruments_sensor.created_date is null;
+"""
+        ),
+        migrations.RunSQL(
+            """with e2 as (
+    SELECT
+      id,
+      created_date,
+      (
+        SELECT p2.created_date
+        FROM instruments_sensor p2
+        WHERE p2.created_date IS NOT NULL AND p2.id >= p1.id
+        ORDER BY p2.id asc
+        LIMIT 1
+      ) AS new_value
+    FROM instruments_sensor p1
+    ORDER BY id
+)
+update instruments_sensor
+set created_date = e2.new_value
+from e2
+where instruments_sensor.id = e2.id and instruments_sensor.created_date is null;
+
+"""
+        ),
+        migrations.RunSQL(
+            "insert into instruments_sensoroninstrument(instrument_id, sensor_id,start_time) select s.instrument_id, s.id, s.created_date from instruments_sensor as s;"
         ),
     ]
