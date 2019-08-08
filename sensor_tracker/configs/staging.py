1ee4c1f1378088f247bed352f1af9f73d627cd4f
xaingling <neolx199@gmail.com>
2019-08-08 17:01:38 -0300

    added conif to staging config

--- sensor_tracker/configs/staging.py
+++ sensor_tracker/configs/staging.py
@@ -33,3 +33,9 @@ MIDDLEWARE = MIDDLEWARE + DEVELOPMENT_MIDDLEWAARE
 
 FORCE_SCRIPT_NAME = '/sensor_tracker_stg'
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
+
