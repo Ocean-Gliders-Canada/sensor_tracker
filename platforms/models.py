76344b3569e95686fad86ec7d45af40dfb623f6c
xaingling <neolx199@gmail.com>
2019-09-26 11:42:01 -0300

    hot fix

--- platforms/models.py
+++ platforms/models.py
@@ -98,9 +98,7 @@ class PlatformDeployment(models.Model):
                                blank=True,
                                help_text="The general comments for the deployment."
                                )
-
-    acknowledgement = models.CharField(
-        max_length=900,
+    acknowledgement = models.TextField(
         help_text="<b>Example:</b> This deployment is supported by funding from NOAA",
         null=True,
         blank=True
