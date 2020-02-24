9c06427e0b2ad94dc9030ab2b25616b00801362b
nicole <xw822833@dal.ca>
2020-02-24 11:09:54 -0400

    remove the repeat set_password

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,6 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomUserCreationForm(UserCreationForm):
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
         user.is_staff = True
         if commit:
             user.save()
