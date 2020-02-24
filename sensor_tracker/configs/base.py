e4175a12109cdc163b4f298a18fb5ea867a9e324
nicole <xw822833@dal.ca>
2020-02-24 14:05:32 -0400

    ST-23 changed static_url

--- sensor_tracker/configs/base.py
+++ sensor_tracker/configs/base.py
@@ -112,10 +112,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/sensor_tracker/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/sensor_tracker/"),
 ]
 SUIT_CONFIG = {
     'ADMIN_NAME': 'Sensor Tracker',
